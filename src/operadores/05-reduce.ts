4416cbed4e660dd4b20409c7492a6580e5a19337
Jesus Camacaro <jesus.camacaro@u-payments.com>
2020-06-09 17:08:05 -0400

    reduce

---
+++ src/operadores/05-reduce.ts
@@ -0,0 +1,60 @@
+import { interval } from "rxjs";
+import { take, reduce, tap } from "rxjs/operators";
+
+
+
+
+
+ /**
+  * REDUCE EN JS 
+  * 
+  * lo que hace es reducir mi arreglo teniendo en cuenta
+  * un valor actual posicion del arrglo donde vaya y un
+  * acumulador valor que se ira retornando y podemos tenerlo
+  */
+const numbers = [1,2,3,4,5];
+
+const totalReducer = ( acumulador: number, valorActual: number ) => {
+
+    return acumulador + valorActual;
+}
+
+/**
+ * Parametros
+ * Funcion a ejecuar
+ * valor inicial
+ */
+const total = numbers.reduce( totalReducer, 0 )
+console.log('Total Arr', total);
+
+/******************************************************************************* 
+ * REDUCE EN RXJS
+ * 
+ * 
+*/
+
+interval(1000)
+.pipe(
+    /**
+     * Esto sirve para tomar valores
+     * del array como cuantas posiciones 
+     * voy a tomar
+     */
+    take(4),
+
+    tap( console.log ),
+
+    /**
+     * Con el Reduce no se tiene ninguna 
+     * emision hasta que no se complete
+     * el observable, o sea el complete
+     * 
+     * parametro inicial funcion
+     * parametro secundario valor inicial
+     */
+    reduce( totalReducer )
+)
+.subscribe({
+    next: val => console.log('next: ', val),
+    complete: () => console.log('Complete')
+})
